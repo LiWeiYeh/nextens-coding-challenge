a0980905c46a006776ba6f0882ebb731d0daf0d5
Levi <liwei_yeh@hotmail.com>
2026-09-21 01:05:04 +0200

    feat: configure environment for API URL and update Vruchtgebruik service

--- Client/rekentool/src/app/services/vruchtgebruik.service.ts
+++ Client/rekentool/src/app/services/vruchtgebruik.service.ts
@@ -3,13 +3,14 @@ import { inject, Injectable } from "@angular/core";
 import { VruchtgebruikRequest } from "../models/vruchtgebruik-request";
 import { VruchtgebruikResponse } from "../models/vruchtgebruik-response";
 import { Observable } from "rxjs";
+import { environment } from "../../environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class VruchtgebruikService {
 
-  private readonly apiUrl = 'https://localhost:7122/api/Vruchtgebruik';
+  private readonly apiUrl = `${environment.apiUrl}/Vruchtgebruik`;
   private readonly http = inject(HttpClient);
 
   constructor() {}
